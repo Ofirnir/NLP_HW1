98e9ec689bdd9fe84f8bb9110a4466c2d992871a
Ofirnir <38858225+Ofirnir@users.noreply.github.com>
2019-03-20 21:55:58 +0200

    Update
    Still need to change the regex

--- hw1.py
+++ hw1.py
@@ -4,13 +4,19 @@ from collections import Counter, OrderedDict, defaultdict
 # n_gram=3
 
 
-def train(techarst, n_gram):
-    #n_counts = defaultdict(Counter)
+def train(text, n_gram):
+    counter = Counter() #fa
+    lines = text.splitlines()
     counter = Counter()
-    for i in range(0, len(techarst) - n_gram + 1):
-        t = techarst[i:i + n_gram]
-        #n_char = techarst[i + n_gram - 1]
-        counter[t] += 1
+
+    for line in lines :
+        line =['<s>', '<s>'] + list(line) + ['<e>','<e>']
+        for i in range(0,len(line) - n_gram + 1 ):
+         #   lin =  +
+            t= line[i:i + n_gram]
+            t2= tuple(t)
+            counter[t2] += 1
+
     return counter
 
 
@@ -20,36 +26,35 @@ def lm(corpus_file, model_file):
         unigram = train(data, 1)
         bigram = train(data, 2)
         trigram = train(data, 3)
-        # print(unigram)
-        # print(bigram)
+        print(unigram)
+        #print(bigram)
         # print(trigram)
         with open(model_file, 'w', encoding="utf8") as outputFile:
             print("3-grams:",file=outputFile,flush=False)
             for j in trigram:
                 chars = bigram[j[0:2]]
-                prob = np.log2(trigram[j] / chars)
-                # print("chars=",i[0: 2],chars , "i=",i,trigram[i], "prob=",prob)
-                print(j, "\t",format("%.5f") % prob, file=outputFile,flush=False)
+                prob = np.log2(trigram[j] / (chars + 1) )
+                #print("chars=",j[0: 2],chars , "i=",j,trigram[j], "prob=",prob)
+                print(j, "\t",format("%.5f") % prob, file=outputFile,flush=False,sep="")
 
             print("\n2-grams:",file=outputFile,flush=False)
             for i in bigram:
                 chars = unigram[i[0]]
-                prob = np.log2(bigram[i] / chars)
+                prob = np.log2(bigram[i] / (chars + 1) )
                 # print("chars=",i[0: 2],chars , "i=",i,trigram[i], "prob=",prob)
-                print(i, "\t", format("%.5f") % prob, file=outputFile, flush=False)
+                print(i, "\t", format("%.5f") % prob, file=outputFile, flush=False,sep="")
             print("\n1-grams:",file=outputFile,flush=False)
 
             for i in unigram:
                 chars = unigram[i]
                 prob = np.log2(chars / len(data))
                 # print("chars=",i[0: 2],chars , "i=",i,trigram[i], "prob=",prob)
-                print(i, "\t", format("%.5f") % prob, file=outputFile, flush=False)
+                print(i, "\t", format("%.5f") % prob, file=outputFile, flush=False,sep="")
 
 def parse(model_file):
     with open(model_file, 'r', encoding="utf8") as modelf:
         data = modelf.readlines()
         parsingDict=defaultdict()
-
         for l in data:
             m = re.search("^(.*) \t (.*)",l)
             if m != None:
@@ -60,7 +65,7 @@ def eval(input_file,model_file,weights):
     with open(input_file,'r',encoding="utf8") as inputf:
         data=inputf.read()
         trigram=train(data,3)
-        dict=  parse(model_file)
+        dict= parse(model_file)
         sum = 0
         for tri in trigram:
             if tri not in dict:
